6a430baea882ddf935eec28c55cca00386acc7fd
akshayghosh-acenet <152206148+akshayghosh-acenet@users.noreply.github.com>
2024-04-17 17:59:46 -0300

    Add files via upload

---
+++ activate_xgoal_env.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# this is to activate the right environment to run ghosh_akshay_ISP_hockey_xgoal_model.ipynb
+
+source activate hockey_env # activate environment
+jupyter notebook # open jupyter notebook
+
